afb137500073a07a094a5b6b7fcd05a413a05094
Mega <Mega@MAMMAD>
2023-02-02 18:33:30 +0330

    Add project files.

---
+++ taghsime_aghsat/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace taghsime_aghsat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kol_tx = new System.Windows.Forms.TextBox();
+            this.darsad_tx = new System.Windows.Forms.TextBox();
+            this.tedat_aghsat_tx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ghest_10_tx = new System.Windows.Forms.TextBox();
+            this.naghd_10_tx = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ghest_50_tx = new System.Windows.Forms.TextBox();
+            this.naghd_50_tx = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ghest_100_tx = new System.Windows.Forms.TextBox();
+            this.naghd_100_tx = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kol_tx
+            // 
+            this.kol_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.kol_tx.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.kol_tx.Location = new System.Drawing.Point(33, 17);
+            this.kol_tx.Name = "kol_tx";
+            this.kol_tx.Size = new System.Drawing.Size(239, 34);
+            this.kol_tx.TabIndex = 0;
+            this.kol_tx.TextChanged += new System.EventHandler(this.kol_tx_TextChanged);
+            this.kol_tx.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kol_tx_KeyPress);
+            // 
+            // darsad_tx
+            // 
+            this.darsad_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.darsad_tx.Location = new System.Drawing.Point(33, 66);
+            this.darsad_tx.Name = "darsad_tx";
+            this.darsad_tx.Size = new System.Drawing.Size(57, 34);
+            this.darsad_tx.TabIndex = 1;
+            this.darsad_tx.Text = "25";
+            this.darsad_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.darsad_tx.TextChanged += new System.EventHandler(this.darsad_tx_TextChanged);
+            // 
+            // tedat_aghsat_tx
+            // 
+            this.tedat_aghsat_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.tedat_aghsat_tx.Location = new System.Drawing.Point(197, 66);
+            this.tedat_aghsat_tx.Name = "tedat_aghsat_tx";
+            this.tedat_aghsat_tx.Size = new System.Drawing.Size(57, 34);
+            this.tedat_aghsat_tx.TabIndex = 2;
+            this.tedat_aghsat_tx.Text = "3";
+            this.tedat_aghsat_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tedat_aghsat_tx.TextChanged += new System.EventHandler(this.tedat_aghsat_tx_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(278, 20);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(62, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "کل مبلغ :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(96, 69);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(68, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "درصد نقد :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(260, 73);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(80, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "تعداد اقساط :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ghest_10_tx);
+            this.groupBox1.Controls.Add(this.naghd_10_tx);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("B Nazanin", 10.2F);
+            this.groupBox1.Location = new System.Drawing.Point(33, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(302, 137);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "رند نسبت به 100,000 ریال";
+            // 
+            // ghest_10_tx
+            // 
+            this.ghest_10_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ghest_10_tx.Location = new System.Drawing.Point(20, 85);
+            this.ghest_10_tx.Name = "ghest_10_tx";
+            this.ghest_10_tx.ReadOnly = true;
+            this.ghest_10_tx.Size = new System.Drawing.Size(189, 34);
+            this.ghest_10_tx.TabIndex = 11;
+            this.ghest_10_tx.Text = "0";
+            this.ghest_10_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // naghd_10_tx
+            // 
+            this.naghd_10_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.naghd_10_tx.Location = new System.Drawing.Point(20, 38);
+            this.naghd_10_tx.Name = "naghd_10_tx";
+            this.naghd_10_tx.ReadOnly = true;
+            this.naghd_10_tx.Size = new System.Drawing.Size(189, 34);
+            this.naghd_10_tx.TabIndex = 10;
+            this.naghd_10_tx.Text = "0";
+            this.naghd_10_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(227, 88);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(50, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "اقساط :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(227, 41);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(36, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "نقد :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ghest_50_tx);
+            this.groupBox2.Controls.Add(this.naghd_50_tx);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("B Nazanin", 10.2F);
+            this.groupBox2.Location = new System.Drawing.Point(33, 269);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(302, 137);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "رند نسبت به 500,000 ریال";
+            // 
+            // ghest_50_tx
+            // 
+            this.ghest_50_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ghest_50_tx.Location = new System.Drawing.Point(20, 85);
+            this.ghest_50_tx.Name = "ghest_50_tx";
+            this.ghest_50_tx.ReadOnly = true;
+            this.ghest_50_tx.Size = new System.Drawing.Size(189, 34);
+            this.ghest_50_tx.TabIndex = 11;
+            this.ghest_50_tx.Text = "0";
+            this.ghest_50_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // naghd_50_tx
+            // 
+            this.naghd_50_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.naghd_50_tx.Location = new System.Drawing.Point(20, 38);
+            this.naghd_50_tx.Name = "naghd_50_tx";
+            this.naghd_50_tx.ReadOnly = true;
+            this.naghd_50_tx.Size = new System.Drawing.Size(189, 34);
+            this.naghd_50_tx.TabIndex = 10;
+            this.naghd_50_tx.Text = "0";
+            this.naghd_50_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(227, 88);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(50, 26);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "اقساط :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(227, 41);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(36, 26);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "نقد :";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ghest_100_tx);
+            this.groupBox3.Controls.Add(this.naghd_100_tx);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Font = new System.Drawing.Font("B Nazanin", 10.2F);
+            this.groupBox3.Location = new System.Drawing.Point(33, 412);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox3.Size = new System.Drawing.Size(302, 137);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "رند نسبت به 1,000,000 ریال";
+            // 
+            // ghest_100_tx
+            // 
+            this.ghest_100_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.ghest_100_tx.Location = new System.Drawing.Point(20, 85);
+            this.ghest_100_tx.Name = "ghest_100_tx";
+            this.ghest_100_tx.ReadOnly = true;
+            this.ghest_100_tx.Size = new System.Drawing.Size(189, 34);
+            this.ghest_100_tx.TabIndex = 11;
+            this.ghest_100_tx.Text = "0";
+            this.ghest_100_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // naghd_100_tx
+            // 
+            this.naghd_100_tx.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.naghd_100_tx.Location = new System.Drawing.Point(20, 38);
+            this.naghd_100_tx.Name = "naghd_100_tx";
+            this.naghd_100_tx.ReadOnly = true;
+            this.naghd_100_tx.Size = new System.Drawing.Size(189, 34);
+            this.naghd_100_tx.TabIndex = 10;
+            this.naghd_100_tx.Text = "0";
+            this.naghd_100_tx.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label8.Location = new System.Drawing.Point(227, 88);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label8.Size = new System.Drawing.Size(50, 26);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "اقساط :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("B Nazanin", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label9.Location = new System.Drawing.Point(227, 41);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label9.Size = new System.Drawing.Size(36, 26);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "نقد :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 566);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tedat_aghsat_tx);
+            this.Controls.Add(this.darsad_tx);
+            this.Controls.Add(this.kol_tx);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kol_tx;
+        private System.Windows.Forms.TextBox darsad_tx;
+        private System.Windows.Forms.TextBox tedat_aghsat_tx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox ghest_10_tx;
+        private System.Windows.Forms.TextBox naghd_10_tx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ghest_50_tx;
+        private System.Windows.Forms.TextBox naghd_50_tx;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox ghest_100_tx;
+        private System.Windows.Forms.TextBox naghd_100_tx;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ taghsime_aghsat/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taghsime_aghsat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Tuple<long, long> ghesting(Int64 kol , Int64 tedad , Int64 darsad , Int64 marja )
+        {
+            Int64 ghest = 0;
+            Int64 naghd = 0;
+            Int64 tmp = 0;
+            tmp = kol * (100 - darsad) / 100;
+            tmp = tmp / tedad;
+            ghest = tmp / marja * marja;
+            naghd = kol - (tedad * ghest);
+
+            return Tuple.Create(naghd, ghest);
+        }
+
+
+        private void kol_tx_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void kol_tx_TextChanged(object sender, EventArgs e)
+        {
+            if (kol_tx.Text != "")
+            {
+                kol_tx.Text = String.Format("{0:N0}", Int64.Parse(kol_tx.Text.Replace(",", "")));
+                kol_tx.SelectionStart = kol_tx.Text.Length;
+                kol_tx.SelectionLength = 0;
+                loading();
+            }
+        }
+
+        void loading()
+        {
+            if (kol_tx.Text != "" & darsad_tx.Text != "" & tedat_aghsat_tx.Text != ""  )
+            {
+                Int64 kol;
+                Int64 darsad;
+                Int64 tedad;
+                if (Int64.TryParse(kol_tx.Text.Replace(",", ""),out kol) & Int64.TryParse(darsad_tx.Text, out darsad) & darsad >= 0 & darsad <= 100 & Int64.TryParse(tedat_aghsat_tx.Text, out tedad ))
+                {
+                    Int64 ghest = 0;
+                    Int64 naghd = 0;
+                    var t = ghesting(kol, tedad, darsad, 100000);
+                    naghd_10_tx.Text = String.Format("{0:N0}", t.Item1);
+                    ghest_10_tx.Text = String.Format("{0:N0}", t.Item2);
+                    t = ghesting(kol, tedad, darsad, 500000);
+                    naghd_50_tx.Text = String.Format("{0:N0}", t.Item1);
+                    ghest_50_tx.Text = String.Format("{0:N0}", t.Item2);
+                    t = ghesting(kol, tedad, darsad, 1000000);
+                    naghd_100_tx.Text = String.Format("{0:N0}", t.Item1);
+                    ghest_100_tx.Text = String.Format("{0:N0}", t.Item2);
+                }
+            }
+        }
+
+        private void tedat_aghsat_tx_TextChanged(object sender, EventArgs e)
+        {
+            loading();
+        }
+
+        private void darsad_tx_TextChanged(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                Int64 darsad = 0;
+                if (Int64.TryParse(darsad_tx.Text , out darsad))
+                {
+                    if (darsad >= 0 & darsad <= 100)
+                        loading();
+                }
+                
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+            
+        }
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+            loading();
+        }
+    }
+}
